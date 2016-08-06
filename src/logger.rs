2ea6d8f5d264640c1d7447adb6f47f9c4511f9e0
VelocityRa <makren67@gmail.com>
2016-08-06 20:34:42 +0300

    Update logger to support lack of arguments

--- src/logger.rs
+++ src/logger.rs
@@ -1,6 +1,9 @@
 
 #[macro_export]
 macro_rules! debug {
+	($fm:expr) => ({
+		println!("[DEBUG]: {}", format!($fm))
+	});
 	($fm:expr, $($arg:expr),*) => ({
 		println!("[DEBUG]: {}", format!($fm, $($arg,)* ))
 	});
@@ -8,6 +11,9 @@ macro_rules! debug {
 
 #[macro_export]
 macro_rules! info {
+	($fm:expr) => ({
+		println!("[INFO]: {}", format!($fm))
+	});
 	($fm:expr, $($arg:expr)*) => ({
 		println!("[INFO]: {}", format!($fm, $($arg,)* ))
 	});
@@ -15,6 +21,9 @@ macro_rules! info {
 
 #[macro_export]
 macro_rules! warn {
+	($fm:expr) => ({
+		println!("[WARN]: {}", format!($fm))
+	});
 	($fm:expr, $($arg:expr),*) => ({
 		println!("[WARN]: {}", format!($fm, $($arg,)* ))
 	});
@@ -22,6 +31,9 @@ macro_rules! warn {
 
 #[macro_export]
 macro_rules! error {
+	($fm:expr) => ({
+		println!("[ERROR]: {}", format!($fm))
+	});
 	($fm:expr, $($arg:expr)*) => ({
 		panic!("[ERROR]: {}", format!($fm, $($arg,)* ))
 	});
